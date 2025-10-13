a7a19909076532182a635ddc843b6f26151d4024
henryomosh <henryomosh7@gmail.com>
2025-10-13 19:49:59 +0300

    changed ads 3

--- app/dashboard/market/page.tsx
+++ app/dashboard/market/page.tsx
@@ -1,6 +1,8 @@
 import Market from "@/components/dashboard-components/market";
 import { fetchAdverts, fetchSchedule } from "@/lib/data";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const adverts = await fetchAdverts();
   const { schedule } = await fetchSchedule();
--- app/dashboard/requests/page.tsx
+++ app/dashboard/requests/page.tsx
@@ -11,6 +11,8 @@ import {
   fetchRequestfDashboard,
 } from "@/lib/data";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
